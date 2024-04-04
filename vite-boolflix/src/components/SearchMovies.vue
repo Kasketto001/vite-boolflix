93ef3fdc288de0b193503f5221709b6792626e82
Kasketto001 <kaskettomaledetto@gmail.com>
2024-04-04 11:48:05 +0200

    Add Languages Flag

--- vite-boolflix/src/components/SearchMovies.vue
+++ vite-boolflix/src/components/SearchMovies.vue
@@ -1,35 +1,40 @@
 <template>
     <div>
-      <input type="text" v-model="searchTerm" placeholder="Cerca film...">
-      <button @click="searchMovies">Cerca</button>
-  
-      <div v-if="searchResults.length > 0">
-        <h2>Risultati della ricerca:</h2>
-        <div v-for="movie in searchResults" :key="movie.id">
-          <h3>{{ movie.title }}</h3>
-          <p><strong>Titolo Originale:</strong> {{ movie.original_title }}</p>
-          <p><strong>Lingua:</strong> {{ movie.original_language }}</p>
-          <p><strong>Voto:</strong> {{ movie.vote_average }}</p>
+        <input type="text" v-model="searchTerm" placeholder="Cerca film...">
+        <button @click="searchMovies">Cerca</button>
+    
+        <div v-if="searchResults.length > 0">
+            <h2>Risultati della ricerca:</h2>
+            <div v-for="movie in searchResults" :key="movie.id">
+                <h3>{{ movie.title }}</h3>
+                <p><strong>Titolo Originale:</strong> {{ movie.original_title }}</p>
+                <img :src="getFlagImageUrl(movie.original_language)" alt="Bandiera lingua">
+                <p><strong>Voto:</strong> {{ movie.vote_average }}</p>
+            </div>
         </div>
-      </div>
     </div>
-  </template>
-  
-  <script>
-  import { ref } from 'vue';
-  import { searchMoviesByTerm } from '../state';
-  
-  export default {
+</template>
+
+<script>
+import { ref } from 'vue';
+import { searchMoviesByTerm } from '../state';
+
+export default {
     setup() {
-      const searchTerm = ref('');
-      const searchResults = ref([]);
-  
-      const searchMovies = async () => {
-        searchResults.value = await searchMoviesByTerm(searchTerm.value);
-      };
-  
-      return { searchTerm, searchResults, searchMovies };
-    },
-  };
-  </script>
-  
+        const searchTerm = ref('');
+        const searchResults = ref([]);
+
+        const searchMovies = async () => {
+            searchResults.value = await searchMoviesByTerm(searchTerm.value);
+        };
+
+        const getFlagImageUrl = (language) => {
+
+
+            return `https://www.unknown.nu/flags/images/${language}-100`;
+        };
+
+        return { searchTerm, searchResults, searchMovies, getFlagImageUrl };
+    }
+};
+</script>
